703f8d54bad1dbe32f41187530a9f3601b4c88b2
Anowond <baroux.maxime2@gmail.com>
2024-01-07 18:10:29 +0100

    fin du t^p

--- includes/podcasts.php
+++ includes/podcasts.php
@@ -12,7 +12,7 @@ $podcasts = [
         "url" => "./sounds/The_Witcher_3_OST_Ciri_Battle_Theme.m4a"
     ],
     [
-        "date" => "07 janvier 2024",
+        "date" => "07 Javier 2024",
         "name" => "Valhalla Calling Me",
         "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iure maxime, minima magni, reiciendis
             officia
@@ -41,6 +41,66 @@ $podcasts = [
         aspernatur enim dolorem illum.",
         "url" => "./sounds/The_Witcher_3_OST_You're..._Immortal.m4a"
     ],
+    [
+        "date" => "07 Javier 2024",
+        "name" => "Chad Battle",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iure maxime, minima magni, reiciendis
+        officia
+        molestiae, culpa libero temporibus nesciunt minus magnam quisquam eius nisi delectus. Voluptate quam, iste animi commodi
+        laboriosam fugit totam eveniet excepturi quas maiores dignissimos, officiis ipsam. Doloribus quia praesentium sit quod
+        aspernatur enim dolorem illum.",
+        "url" => "./sounds\Dr._Livesey_vs_Gigachad_PIANO_BATTLE.m4a"
+    ],
+    [
+        "date" => "07 Javier 2024",
+        "name" => "Bête du Gevaudan",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iure maxime, minima magni, reiciendis
+        officia
+        molestiae, culpa libero temporibus nesciunt minus magnam quisquam eius nisi delectus. Voluptate quam, iste animi commodi
+        laboriosam fugit totam eveniet excepturi quas maiores dignissimos, officiis ipsam. Doloribus quia praesentium sit quod
+        aspernatur enim dolorem illum.",
+        "url" => "./sounds\POWERWOLF_Bête_du_Gévaudan.m4a"
+    ],
+    [
+        "date" => "07 Javier 2024",
+        "name" => "Darth Revan Theme",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iure maxime, minima magni, reiciendis
+        officia
+        molestiae, culpa libero temporibus nesciunt minus magnam quisquam eius nisi delectus. Voluptate quam, iste animi commodi
+        laboriosam fugit totam eveniet excepturi quas maiores dignissimos, officiis ipsam. Doloribus quia praesentium sit quod
+        aspernatur enim dolorem illum.",
+        "url" => "./sounds\Star_Wars_Darth_Revan_Theme.m4a"
+    ],
+    [
+        "date" => "07 Javier 2024",
+        "name" => "Duel of Fates",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iure maxime, minima magni, reiciendis
+        officia
+        molestiae, culpa libero temporibus nesciunt minus magnam quisquam eius nisi delectus. Voluptate quam, iste animi commodi
+        laboriosam fugit totam eveniet excepturi quas maiores dignissimos, officiis ipsam. Doloribus quia praesentium sit quod
+        aspernatur enim dolorem illum.",
+        "url" => "./sounds\Star_Wars_Duel_of_The_Fates.m4a"
+    ],
+    [
+        "date" => "07 Javier 2024",
+        "name" => "Zizz Wannabe",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iure maxime, minima magni, reiciendis
+        officia
+        molestiae, culpa libero temporibus nesciunt minus magnam quisquam eius nisi delectus. Voluptate quam, iste animi commodi
+        laboriosam fugit totam eveniet excepturi quas maiores dignissimos, officiis ipsam. Doloribus quia praesentium sit quod
+        aspernatur enim dolorem illum.",
+        "url" => "./sounds\Tevvez_Legend.m4a"
+    ],
+    [
+        "date" => "07 Javier 2024",
+        "name" => "Ladies of the Woods",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iure maxime, minima magni, reiciendis
+        officia
+        molestiae, culpa libero temporibus nesciunt minus magnam quisquam eius nisi delectus. Voluptate quam, iste animi commodi
+        laboriosam fugit totam eveniet excepturi quas maiores dignissimos, officiis ipsam. Doloribus quia praesentium sit quod
+        aspernatur enim dolorem illum.",
+        "url" => "./sounds\The_Witcher_3_OST_Ladies_of_the_Woods.opus"
+    ]
 
 ]
     ?>
